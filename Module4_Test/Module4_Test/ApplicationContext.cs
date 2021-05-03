f7c9b36d09d473e34d8b441a2b974d23035ea565
Vira Vostrikova <vvostrikova@csod.com>
2021-05-03 14:57:33 +0300

    Initial commit

---
+++ Module4_Test/Module4_Test/ApplicationContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Module4_Test
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<EmployeeProject> EmployeeProjects { get; set; }
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging().LogTo(Console.WriteLine, LogLevel.Information);
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new OfficeConfiguration());
+            modelBuilder.ApplyConfiguration(new ProjectConfiguration());
+            modelBuilder.ApplyConfiguration(new TitleConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeProjectConfiguration());
+            modelBuilder.ApplyConfiguration(new ClientConfiguration());
+        }
+    }
+}
---
+++ Module4_Test/Module4_Test/LazyLoadingSamples.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Module4_Test
+{
+    public class LazyLoadingSamples
+    {
+        private readonly ApplicationContext _context;
+
+        public LazyLoadingSamples(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        /*public async Task Query1()
+        {
+            var employees = await _context.Employees
+                .Select(x => new { Name = $"{x.FirstName} {x.LastName}", Title = x.Title.Name, Location = x.Office.Location })
+                .ToListAsync();
+
+            Console.WriteLine("Employee data");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"Employee name: {employee.Name}. Employee job title: {employee.Title}. Employee location: {employee.Location}.");
+            }
+        }*/
+    }
+}
